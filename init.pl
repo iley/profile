4409a338ddfc7697e24d36501174d24f4bad9a79
Ilya Strukov <iley@iley.ru>
2012-08-26 22:14:45 +0400

    i3: first config

--- init.pl
+++ init.pl
@@ -14,6 +14,7 @@ my @groups = (
             'XCompose',
             'bashrc',
             'gitconfig',
+            'i3',
             'inputrc',
             'mplayer',
             'oh-my-zsh',
