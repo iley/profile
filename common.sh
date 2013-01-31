c87b063c3f0be319f2357e10fd74711c6a71c5e3
Ilya Strukov <iley@iley.ru>
2013-01-31 10:43:45 +0400

    shell: add say() function

--- common.sh
+++ common.sh
@@ -33,5 +33,7 @@ if [ -e ~/.rvm/scripts ]; then
     source $HOME/.rvm/scripts/rvm
 fi
 
+say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
+
 # load machine-specific setting at the very end
 source $PROFILE/local.sh
