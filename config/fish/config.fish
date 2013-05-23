515b189c7a12c46beb9863a82d327a62e4a97d49
Ilya Strukov <iley@iley.ru>
2013-05-23 10:49:47 +0400

    add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,25 @@
+set fish_greeting ""
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+
+function fish_prompt
+  set last_status $status
+  printf '%s@%s ' (whoami) (hostname|cut -d . -f 1)
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
