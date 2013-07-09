ed9799e6ecfd761212fdfa6ecdac2fc5d5b26f8b
Ilya Strukov <iley@iley.ru>
2013-07-09 11:21:37 +0400

    shell: add gi() function

--- common.sh
+++ common.sh
@@ -37,7 +37,9 @@ if [ -e ~/.rvm/scripts ]; then
     source $HOME/.rvm/scripts/rvm
 fi
 
-say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
+function say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
+
+function gi() { curl http://gitignore.io/api/$@ ;}
 
 source $PROFILE/login.sh
 # load machine-specific setting at the very end
