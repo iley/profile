982ab2f5ed6d0368bfefc0f0b0ba9b841f92ac5c
Ilya Strukov <iley@iley.ru>
2013-09-19 14:21:22 +0400

    i3pystatus: change load format

--- i3pystatus.py
+++ i3pystatus.py
@@ -3,7 +3,7 @@ from . import Status
 status = Status(standalone=True)
 
 status.register("clock", format="%a %-d %b %X")
-status.register("load")
+status.register("load", format="load (1m): {avg1}")
 status.register("alsa", format="♪ {volume}")
 
 status.run()
