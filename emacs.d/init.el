8fbba293827f4a2a22ecfeeea3a98243a792b7fc
Ilya Strukov <iley@iley.ru>
2013-10-04 12:08:37 +0400

    emacs: colors

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -29,10 +29,11 @@
 (global-set-key [(super c)] 'clipboard-kill-ring-save)
 (global-set-key [(super v)] 'clipboard-yank)
 
+(setq solarized-termcolors 256)
+
 (defun setup-theme ()
   (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-  (let ((custom-safe-themes '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
-    (load-theme 'solarized-dark)))
+  (load-theme 'solarized-dark t))
 
 (defun setup-package ()
   (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -122,6 +123,24 @@
 
 (global-set-key [C-S-right] 'shift-right)
 (global-set-key [C-S-left] 'shift-left)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(cursor-color "#839496")
+ '(custom-safe-themes (quote ("146aeadf49a8e27f28d7aebb7640f41fe75c1b9d4e94f38113860d08afd21e37" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color "#839496"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (provide 'init)
 
