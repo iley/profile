9650f0779fdd1eac9260f9e09847fc6770349ed9
Ilya Strukov <iley@iley.ru>
2013-10-24 12:18:06 +0400

    i3pystatus: add weather indicator

--- i3pystatus.py
+++ i3pystatus.py
@@ -5,5 +5,8 @@ status = Status(standalone=True)
 status.register("clock", format="%a %-d %b %X")
 status.register("load", format="load (1m): {avg1}")
 status.register("alsa", format="♪ {volume}")
+status.register("weather",
+                location_code="RSXX0063:1",
+                format="Weather in Moscow: {current_temp}")
 
 status.run()
