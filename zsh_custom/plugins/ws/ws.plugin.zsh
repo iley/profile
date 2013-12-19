b139eb084dd3bdac67e0f334339f97b5dd8bac56
Ilya Strukov <iley@iley.ru>
2013-12-19 16:37:18 +0400

    ws: make ohmyzsh plugin, implement mkvirtualenv, rmvirtualenv

--- zsh_custom/plugins/ws/ws.plugin.zsh
+++ zsh_custom/plugins/ws/ws.plugin.zsh
@@ -2,7 +2,7 @@ if [ -z "$WORKON_HOME" ]; then
     export WORKON_HOME="$HOME/.virtualenvs"
 fi
 
-function workon () {
+workon () {
     virtualenv="$1"
     if [ -z "$virtualenv" ]; then
         echo "Usage: workon env_name" >&2
@@ -18,9 +18,47 @@ function workon () {
     . "$WORKON_HOME/$virtualenv/bin/activate"
 }
 
+mkvirtualenv () {
+    name="$1"
+    shift
+    if [ -z "$name" ]; then
+        echo "Usage: mkvirtualenv name [args]" >&2
+        return 1
+    fi
+    env_path="$WORKON_HOME/$name"
+    if [ -e "$env_path" ]; then
+        echo "virtualenv '$env_path' already exists" >&2
+        return 1
+    fi
+    mkdir -p "$env_path"
+    virtualenv $* "$env_path"
+}
+
+rmvirtualenv () {
+    name="$1"
+    if [ -z "$name" ]; then
+        echo "Usage: rmvirtualenv name" >&2
+        return 1
+    fi
+    env_path="$WORKON_HOME/$name"
+    if [ ! -e "$env_path" ]; then
+        echo "virtualenv '$env_path' does not exists" >&2
+        return 1
+    fi
+
+    read -q "REPLY?Delete virtualenv '$env_path'? [y/N] " -c -n 1
+    echo
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        rm -rf "$env_path"
+    else
+        echo "action aborted" >&2
+        return 1
+    fi
+}
+
 # TODO: mkvirtualenv, rmvirtualenv
 
-function ws () {
+ws () {
     workspace="$1"
     if [ -z "$workspace" ]; then
         echo "Usage: ws workspace_name" >&2
