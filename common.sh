6a951a2350d7d9d579174935a123c8781e40c259
Ilya Strukov <iley@iley.ru>
2014-08-23 00:07:22 -0700

    common.sh: set $GOPATH

--- common.sh
+++ common.sh
@@ -43,6 +43,12 @@ if [ -e ~/.rvm/scripts ]; then
     source $HOME/.rvm/scripts/rvm
 fi
 
+if [ -d ~/code/go ]; then
+    export GOPATH="$HOME/code/go"
+else
+    export GOPATH="$HOME/go"
+fi
+
 function say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
 
 source $PROFILE/login.sh
