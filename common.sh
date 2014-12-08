acb29d5f0747bb6a2a8c8eb05025cd525a9f0044
Ilya Strukov <istrukov@google.com>
2014-12-08 15:36:31 +0100

    shell: meld for g4

--- common.sh
+++ common.sh
@@ -51,6 +51,10 @@ fi
 
 function say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
 
+# use meld for g4 diff
+if [ -n "$DISPLAY" ] ; then export G4MULTIDIFF=1 ; fi
+export P4DIFF='bash -c "meld \${@/#:/--diff}" padding-to-occupy-argv0'
+
 source $PROFILE/login.sh
 # load machine-specific setting at the very end
 source $PROFILE/local.sh
