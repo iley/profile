fe230c4b8e7e0abb18ae9ef9f7dbc291d8db8135
Ilya Strukov <iley@iley.ru>
2015-07-13 09:39:14 +0200

    sh: up function

--- common.sh
+++ common.sh
@@ -51,6 +51,23 @@ fi
 
 function say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
 
+
+function up() {
+  local start="$(pwd)"
+  local target="$1"
+  if [[ -z "$target" ]]; then
+    echo "Usage: up DIR" 1>&2
+    return
+  fi
+  while [[ "$(basename $(pwd))" != "$target" ]] && [[ "$(pwd)" != "/" ]]; do
+    cd ..
+  done
+  if [[ "$(pwd)" == "/" ]]; then
+    echo "Directory $target not found in path" 1>&2
+    cd "$start"
+  fi
+}
+
 # use meld for g4 diff
 if [ -n "$DISPLAY" ] ; then export G4MULTIDIFF=1 ; fi
 export P4DIFF='bash -c "meld \${@/#:/--diff}" padding-to-occupy-argv0'
