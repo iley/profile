59186aab8267f11f4c65dcfabf4ad0bba593068a
Ilya Strukov <iley@iley.ru>
2015-10-12 22:13:09 +0200

    update shell aliases

--- alias.sh
+++ alias.sh
@@ -5,23 +5,11 @@ elif [[ $(uname -s) = 'Darwin' ]]; then
     alias ls='ls -G'
 fi
 
+if [[ command -v ack-grep >/dev/null 2>&1]]; then
+    alias ack='ack-grep'
+fi
+
 alias xc='xclip -i -selection clipboard'
 alias xp='xclip -o -selection clipboard'
 
-alias py='ack-grep --python'
-alias ack='ack-grep'
-
-alias ncal='ncal -M'
-
-alias bell='echo -en "\a"'
-
-# git
-alias g='git'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gst='git status -sb'
-
-# pseudo-aliases
-function lt() { ls -ltrsa "$@" | tail; }
-function psgrep() { ps axuf | grep -v grep | grep "$@" -i --color=auto; }
-function fname() { find . -iname "*$@*"; }
+alias vi=vim
