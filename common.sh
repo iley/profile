9a2e78a33ed85ca87f1f9392bda00e253a6fde05
Ilya Strukov <iley@iley.ru>
2015-11-28 10:18:36 +0100

    shell: add "desk" helper function; clean up

--- common.sh
+++ common.sh
@@ -1,46 +1,24 @@
-export GPGKEY=DCC2E057
+PROFILE=$HOME/profile
+
 export EDITOR=vim
 export PAGER=less
 export LESS="-iMSx4 -FXR"
 export PATH=$PATH:~/bin
-export DEBEMAIL="iley@iley.ru"
-export DEBFULLNAME="Ilya Strukov"
-export GDK_USE_XFT=1
-export QT_XFT=true
 # required for proper XCompose work in GTK apps
 export GTK_IM_MODULE=xim
 export ANSIBLE_NOCOWS=1
 
-if [ -e "/usr/local/heroku" ]; then
-    export PATH="/usr/local/heroku/bin:$PATH"
-fi
-
-if [ -e "$HOME/.local/bin" ]; then
+if [[ -e "$HOME/.local/bin" ]]; then
     export PATH=$PATH:$HOME/.local/bin
 fi
 
-if [ -e "$HOME/.npm-packages/bin" ]; then
+if [[ -e "$HOME/.npm-packages/bin" ]]; then
     export PATH=$PATH:$HOME/.npm-packages/bin
-fi
-
-PROFILE=$HOME/profile
-EXT=$PROFILE/ext
-
-# common aliases for all shells
-source $PROFILE/alias.sh
-
-# perlbrew
-if [ -e ~/perl5 ]; then
-    source ~/perl5/perlbrew/etc/bashrc
 fi
 
 # set $TERM to make tmux work properly
 if [[ "$TERM" != 'screen-256color' ]]; then
     export TERM=xterm-256color
-fi
-
-if [ -e ~/.rvm/scripts ]; then
-    source $HOME/.rvm/scripts/rvm
 fi
 
 if [ -d ~/code/go ]; then
@@ -49,29 +27,12 @@ else
     export GOPATH="$HOME/go"
 fi
 
-function say() { if [[ "${1}" =~ -[a-z]{2} ]]; then local lang=${1#-}; local text="${*#$1}"; else local lang=${LANG%_*}; local text="$*";fi; mplayer "http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${text}" &> /dev/null ; }
-
-
-function up() {
-  local start="$(pwd)"
-  local target="$1"
-  if [[ -z "$target" ]]; then
-    echo "Usage: up DIR" 1>&2
-    return
-  fi
-  while [[ "$(basename $(pwd))" != "$target" ]] && [[ "$(pwd)" != "/" ]]; do
-    cd ..
-  done
-  if [[ "$(pwd)" == "/" ]]; then
-    echo "Directory $target not found in path" 1>&2
-    cd "$start"
-  fi
-}
-
 # use meld for g4 diff
 if [ -n "$DISPLAY" ] ; then export G4MULTIDIFF=1 ; fi
 export P4DIFF='bash -c "meld \${@/#:/--diff}" padding-to-occupy-argv0'
 
-source $PROFILE/login.sh
+source "$PROFILE/helpers.sh"
+source "$PROFILE/alias.sh"
+source "$PROFILE/login.sh"
 # load machine-specific setting at the very end
-source $PROFILE/local.sh
+source "$PROFILE/local.sh"
---
+++ helpers.sh
@@ -0,0 +1,87 @@
+if [ -z "$WORKON_HOME" ]; then
+    export WORKON_HOME="$HOME/.virtualenvs"
+fi
+
+desk () {
+  local desk_name="$1"
+  if [[ -z "$desk_name" ]]; then
+    echo "Usage: desk desk_name" >&2
+    return 1
+  fi
+  local desk_file="$HOME/.desks/$desk_name.sh"
+  if ! [[ -e "$desk_file" ]]; then
+    echo "Desk file '$desk_file' not found" >&2
+    return 1
+  fi
+  . "$desk_file"
+}
+
+workon () {
+    local virtualenv="$1"
+    if [[ -z "$virtualenv" ]]; then
+        echo "Usage: workon env_name" >&2
+        return 1
+    fi
+    if ! [[ -e "$WORKON_HOME/$virtualenv" ]]; then
+        echo "Virtualenv '$virtualenv' does not exist" >&2
+        return 1
+    fi
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        deactivate
+    fi
+    . "$WORKON_HOME/$virtualenv/bin/activate"
+}
+
+mkvirtualenv () {
+    local name="$1"
+    shift
+    if [[ -z "$name" ]]; then
+        echo "Usage: mkvirtualenv name [args]" >&2
+        return 1
+    fi
+    local env_path="$WORKON_HOME/$name"
+    if [[ -e "$env_path" ]]; then
+        echo "virtualenv '$env_path' already exists" >&2
+        return 1
+    fi
+    mkdir -p "$env_path"
+    virtualenv $* "$env_path"
+}
+
+rmvirtualenv () {
+    local name="$1"
+    if [ -z "$name" ]; then
+        echo "Usage: rmvirtualenv name" >&2
+        return 1
+    fi
+    local env_path="$WORKON_HOME/$name"
+    if [ ! -e "$env_path" ]; then
+        echo "virtualenv '$env_path' does not exists" >&2
+        return 1
+    fi
+
+    read -q "REPLY?Delete virtualenv '$env_path'? [y/N] " -c -n 1
+    echo
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+        rm -rf "$env_path"
+    else
+        echo "action aborted" >&2
+        return 1
+    fi
+}
+
+function up() {
+  local start="$(pwd)"
+  local target="$1"
+  if [[ -z "$target" ]]; then
+    echo "Usage: up DIR" 1>&2
+    return
+  fi
+  while [[ "$(basename $(pwd))" != "$target" ]] && [[ "$(pwd)" != "/" ]]; do
+    cd ..
+  done
+  if [[ "$(pwd)" == "/" ]]; then
+    echo "Directory $target not found in path" 1>&2
+    cd "$start"
+  fi
+}
