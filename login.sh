8b7240ffcc08da8918bc5194abf8b1ad706c55d3
Ilya Strukov <iley@iley.ru>
2016-08-01 08:58:57 +0200

    shell: change start-up behaviour

--- login.sh
+++
@@ -1,7 +0,0 @@
-if command -v fortune >/dev/null 2>&1; then
-    if command -v cowsay >/dev/null 2>&1; then
-        fortune | cowsay -f $(ls /usr/share/cowsay/cows | sort -R | head -1)
-    else
-        fortune
-    fi
-fi
