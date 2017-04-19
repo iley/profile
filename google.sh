619755ed0d9dc36c1a02fde5c95cc90c5402e76c
Ilya Strukov <iley@iley.ru>
2017-04-19 15:33:03 +0200

    cleanup google.sh, add another alias

--- google.sh
+++ google.sh
@@ -4,32 +4,33 @@ source ~/.x20_tools.sh  # Updated by cron.
 
 DEFAULT_USER=istrukov
 
-BLAZE_RUN='/home/build/nonconf/google3/devtools/blaze/scripts/blaze-run.sh'
-IBLAZE='/google/data/ro/teams/iblaze/iblaze'
+blaze_run='/home/build/nonconf/google3/devtools/blaze/scripts/blaze-run.sh'
+iblaze='/google/data/ro/teams/iblaze/iblaze'
 
-alias blaze-run="$BLAZE_RUN"
-alias iblaze="$IBLAZE"
+alias annealing="/google/data/ro/teams/annealing/live/annealing"
+alias blaze-run="$blaze_run"
 alias build_cleaner='/google/data/ro/teams/social-backend/build_cleaner'
 alias cdd='/google/data/ro/buildstatic-ro/projects/chubby/cdd/cdd'
-alias sandman='/google/data/ro/projects/sandman/sandman'
+alias iblaze="$iblaze"
+alias patchpanel='/google/data/ro/projects/patchpanel/patchpanel'
 alias prodplan='/google/data/ro/projects/prodplan/prodplan'
 alias prodspec='/google/data/ro/teams/prodspec/prodspec'
-alias patchpanel='/google/data/ro/projects/patchpanel/patchpanel'
+alias sandman='/google/data/ro/projects/sandman/sandman'
 
 export X20HOME="/google/data/rw/users/${USER:0:2}/$USER"
 
 function ui-tests {
-  $BLAZE_RUN //storage/x20/ui:karma_local
+  $blaze_run //storage/x20/ui:karma_local
 }
 
 function ui-debug {
-  tmux split-window "$BLAZE_RUN //storage/x20/webexporter:webexporter -- --x20web_debug --logtostderr --x20_environment=dev"
-  $IBLAZE run //storage/x20/ui/dev_server:dev_server
+  tmux split-window "$blaze_run //storage/x20/webexporter:webexporter -- --x20web_debug --logtostderr --x20_environment=dev"
+  $iblaze run //storage/x20/ui/dev_server:dev_server
 }
 
 function ui-vdebug {
-  tmux split-window -h "$BLAZE_RUN //storage/x20/webexporter:webexporter -- --x20web_debug --logtostderr --x20_environment=dev"
-  $IBLAZE run //storage/x20/ui/dev_server:dev_server
+  tmux split-window -h "$blaze_run //storage/x20/webexporter:webexporter -- --x20web_debug --logtostderr --x20_environment=dev"
+  $iblaze run //storage/x20/ui/dev_server:dev_server
 }
 
 function ui-rollout {
