e5b803973aa5c200341965706a57178abf2ba2af
Ilya Strukov <iley@iley.ru>
2019-02-16 10:41:03 +0100

    add cmake to install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install zsh vim-nox atool build-essential tmux python3-dev libboost-all-dev libicu-dev -y
+sudo apt-get install zsh vim-nox atool build-essential tmux python3-dev libboost-all-dev libicu-dev cmake -y
