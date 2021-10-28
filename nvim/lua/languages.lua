671021b37e93c12b1529c67dfd275438549ab605
Ilya Strukov <istrukov@stripe.com>
2021-10-28 09:40:01 +0100

    neovim: disable java lsp

--- nvim/lua/languages.lua
+++ nvim/lua/languages.lua
@@ -38,25 +38,3 @@ lspconfig.gopls.setup{on_attach=completion.on_attach}         -- Go
 lspconfig.pyright.setup{on_attach=completion.on_attach}       -- Python
 lspconfig.rust_analyzer.setup{on_attach=completion.on_attach} -- Rust
 lspconfig.tsserver.setup{on_attach=completion.on_attach}      -- TypeScript
-
--- Ruby
--- Install Solargraph with gem install --user-install solargraph
-lspconfig.solargraph.setup{on_attach=completion.on_attach}
-
--- To use the Java language server
--- 1. Download JDT LS at https://github.com/eclipse/eclipse.jdt.ls
--- 2. Install appropriate version of JDK (e.g. JDK 11 for jdtls 1.0.0)
--- 3. Build it: `./mvnw clean install -DskipTests`
--- 4. Update paths here.
-local jdtls_dir = tostring(vim.fn.getenv("HOME")) .. "/java/eclipse.jdt.ls"
-local jdtls_jar = jdtls_dir .. "/org.eclipse.jdt.ls.product/target/repository/plugins/" ..
-    "org.eclipse.equinox.launcher_1.6.100.v20201223-0822.jar"
--- TODO: Auto-detect OS.
-local jdtls_config = jdtls_dir .. "/org.eclipse.jdt.ls.product/target/repository/config_mac"
-
-vim.fn.setenv("JAR", jdtls_jar)
-vim.fn.setenv("JDTLS_CONFIG", jdtls_config)
-vim.fn.setenv("WORKSPACE", vim.fn.getenv("HOME") .. "/java")
-vim.fn.setenv("GRADLE_HOME", "/opt/homebrew/opt/gradle")
-
-lspconfig.jdtls.setup{}
