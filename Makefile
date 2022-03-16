1e20a19688d4c6f461709bb961eae43ddc636cdf
Ilya Strukov <iley@iley.ru>
2022-03-16 11:52:58 +0000

    vim go config

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 HOME_SRC=bashrc gitconfig_global gitignore_global tmux.conf zshrc gdbinit
 HOME_DST=$(addprefix $(HOME)/.,$(HOME_SRC))
+NODOT_SRC=revive.toml
+NODOT_DST=$(addprefix $(HOME)/,$(NODOT_SRC))
 
 CONFIG_SRC=i3 nvim
 CONFIG_DST=$(addprefix $(HOME)/.config/,$(CONFIG_SRC))
@@ -10,12 +12,15 @@ OHMYZSH=~/.oh-my-zsh
 
 all: links ~/.localrc ~/.gitconfig
 
-links: $(HOME_DST) $(CONFIG_DST) $(OHMYZSH)
+links: $(HOME_DST) $(NODOT_DST) $(CONFIG_DST) $(OHMYZSH)
 
 $(OHMYZSH):
 	git clone https://github.com/robbyrussell/oh-my-zsh.git $@
 
 $(HOME_DST): $(HOME)/.%: %
+	ln -sf $(abspath $<) $@
+
+$(NODOT_DST): $(HOME)/%: %
 	ln -sf $(abspath $<) $@
 
 $(CONFIG_DST): $(HOME)/.config/%: %
--- nvim/lua/languages.lua
+++ nvim/lua/languages.lua
@@ -3,7 +3,37 @@ require('go').setup{} -- setup nvim-go
 local lspconfig = require('lspconfig')
 local completion = require('completion')
 
-lspconfig.clangd.setup{on_attach=completion.on_attach}        -- C & C++
+require('go').setup({
+    auto_format = true,
+    auto_lint = true,
+    -- linters: revive, errcheck, staticcheck, golangci-lint
+    linter = 'revive',
+    -- linter_flags: e.g., {revive = {'-config', '/path/to/config.yml'}}
+    linter_flags = {},
+    -- lint_prompt_style: qf (quickfix), vt (virtual text)
+    lint_prompt_style = 'vt',
+    -- formatter: goimports, gofmt, gofumpt
+    formatter = 'goimports',
+    -- test flags: -count=1 will disable cache
+    test_flags = {'-v'},
+    test_timeout = '30s',
+    test_env = {},
+    -- show test result with popup window
+    test_popup = true,
+    test_popup_width = 80,
+    test_popup_height = 10,
+    -- test open
+    test_open_cmd = 'edit',
+    -- struct tags
+    tags_name = 'json',
+    tags_options = {'json=omitempty'},
+    tags_transform = 'snakecase',
+    tags_flags = {'-skip-unexported'},
+    -- quick type
+    quick_type_flags = {'--just-types'},
+})
+
+-- lspconfig.clangd.setup{on_attach=completion.on_attach}        -- C & C++
 lspconfig.gopls.setup{on_attach=completion.on_attach}         -- Go
 lspconfig.pyright.setup{on_attach=completion.on_attach}       -- Python
 lspconfig.rust_analyzer.setup{on_attach=completion.on_attach} -- Rust
