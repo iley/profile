b169d64062aaa0006fffa5f9f6f4d9e18e60b35d
Ilya Strukov <iley@iley.ru>
2022-12-27 11:33:31 +0000

    hammerspoon: basic Kamaji funcitonality

--- hammerspoon/Spoons/Kamaji.spoon/init.lua
+++ hammerspoon/Spoons/Kamaji.spoon/init.lua
@@ -13,22 +13,90 @@ end
 local icons = dofile(scriptPath() .. "/icons.lua")
 
 local EXPECTED_DEVICE_NAME = "SparkFun Pro Micro"
+local CHANNEL_LEDS = 0
+local CHANNEL_BUZZER = 1
+local NOTE_TO_PLAYER = {
+  [24] = 0, [25] = 1, [26] = 2, [27] = 3,
+  [28] = 4, [29] = 5, [30] = 6, [31] = 7,
+}
+-- for blinking LEDs
+local PLAYER_TO_NOTE = {
+  [0] = 24, [1] = 25, [2] = 26, [3] = 27,
+  [4] = 28, [5] = 29, [6] = 30, [7] = 31,
+}
+local MAX_VELOCITY = 127
 
 local state = {
   device = nil,
   menu = nil,
+  lastDeviceList = {},
+  alertsMuted = false,
+  blinkOnPress = true,
 }
 
+local function blinkLED(player, durationSec)
+  if state.device == nil then
+    return
+  end
+
+  local note = PLAYER_TO_NOTE[player]
+  if note == nil then
+    return
+  end
+
+  local metadata = {
+    channel = CHANNEL_LEDS,
+    note = note,
+    velocity = MAX_VELOCITY,
+  }
+  state.device:sendCommand("noteOn", metadata)
+
+  local function sendNoteOff()
+    local metadata = {
+      channel = CHANNEL_LEDS,
+      note = note,
+      velocity = 0,
+    }
+    state.device:sendCommand("noteOff", metadata)
+  end
+
+  hs.timer.doAfter(durationSec, sendNoteOff)
+end
+
+local function showAlert(message)
+  if not state.alertsMuted then
+    hs.alert.show(message)
+  end
+end
+
+local function handleMute()
+  state.alertsMuted = not state.alertsMuted
+end
+
+local function handleBlinkOnPress()
+  state.blinkOnPress = not state.blinkOnPress
+end
+
 local function handleBlink()
-  -- TODO
+  blinkLED(0, 1) -- player 0, 1 second
 end
 
 local function getMenuItems()
   local menuItems = {
+    {
+      title = "Mute alerts",
+      fn = handleMute,
+      checked = state.alertsMuted,
+    },
+    {
+      title = "Blink on button press",
+      fn = handleBlinkOnPress,
+      checked = state.blinkOnPress,
+    },
     { 
-      title = "Blink LED",
+      title = "Blink Player 1 LED",
       fn = handleBlink,
-    }
+    },
   }
   return menuItems
 end
@@ -46,19 +114,57 @@ local function hideMenu()
   end
 end
 
+local function handlePushDown(player)
+  -- a player button was pushed down
+  showAlert(string.format("Player %d button pushed", player + 1))
+  if state.blinkOnPress then
+    blinkLED(player, 0.5)
+  end
+end
+
+local function handlePushUp(player)
+  -- a player button was pushed up after previously being pushed down
+end
+
+local function handleMidiEvent(object, deviceName, commandType, description, metadata)
+  if commandType == "noteOn" then
+    note = metadata.note
+    player = NOTE_TO_PLAYER[note]
+    if player ~= nil then
+      handlePushDown(player)
+    end
+  elseif commandType == "noteOff" then
+    note = metadata.note
+    player = NOTE_TO_PLAYER[note]
+    if player ~= nil then
+      handlePushUp(player)
+    end
+  end
+end
+
 local function handleDevicesUpdate(devices, virtualDevices)
+  if table.concat(devices, "|") == table.concat(state.lastDeviceList, "|") then
+    -- the device list did not change, ignore update
+    return
+  end
+
   state.device = nil
   for _, deviceName in ipairs(devices) do
     if deviceName == EXPECTED_DEVICE_NAME then
       state.device = hs.midi.new(deviceName)
+      state.device:callback(handleMidiEvent)
     end
   end
 
   if state.device == nil then
     hideMenu()
   else
+    -- this message cannot be muted
+    hs.alert.show("Kamaji device connected")
     showMenu()
   end
+
+  state.lastDeviceList = devices
 end
 
 function spoon:init()
