f9a8400065780a75646afa82de494f4b16bb408e
Ilya Strukov <ilya@istrukov.com>
2023-08-07 10:05:21 +0100

    neovim: fix a deprecation warning

--- nvim/lua/languages.lua
+++ nvim/lua/languages.lua
@@ -84,7 +84,7 @@ cmp.setup.cmdline(':', {
 	})
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lspconfig.clangd.setup{capabilities=capabilities}
 lspconfig.gopls.setup{capabilities=capabilities}
