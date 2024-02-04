1988abaea264665b29325abaef14dc12cc819d4e
Ilya Strukov <ilya@istrukov.com>
2024-02-04 10:11:55 +0000

    nvim stuff

--- nvim/lua/bindings.lua
+++ nvim/lua/bindings.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', ';', ':', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true })
