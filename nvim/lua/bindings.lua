57905d80dbe48a18da9023788b1fd38175ac8d34
Ilya Strukov <ilya@istrukov.com>
2024-04-21 11:54:51 +0100

    add telescope to neovim

--- nvim/lua/bindings.lua
+++ nvim/lua/bindings.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_set_keymap('n', ';', ':', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true })
